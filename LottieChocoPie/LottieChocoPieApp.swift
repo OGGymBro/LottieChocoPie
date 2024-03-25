4112444057b3a62298a7e0fa445b0d171106d448
OGGymBro <143493367+OGGymBro@users.noreply.github.com>
2024-03-25 22:28:32 +0530

    Initial Commit

---
+++ LottieChocoPie/LottieChocoPieApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieChocoPieApp.swift
+//  LottieChocoPie
+//
+//  Created by Joaquim Menezes on 25/03/24.
+//
+
+import SwiftUI
+
+@main
+struct LottieChocoPieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
